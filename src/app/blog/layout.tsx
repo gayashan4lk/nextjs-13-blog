eede3a113cea24831b3f3753059be2b90f719bb5
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-02-28 22:58:59 +0530

    install tailwind

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -5,8 +5,21 @@ export default function BlogLayout({
 }) {
 	return (
 		<>
-			<header>DevBlog</header>
-			<nav>Topics</nav>
+			<header>
+				<span>Dev Blog</span>
+				<span>News</span>
+				<span>Contact</span>
+				<span>About</span>
+			</header>
+			<nav>
+				<h3>C#</h3>
+				<h3>.NET</h3>
+				<h3>ASP.NET Core</h3>
+				<h3>JavaScript</h3>
+				<h3>React.JS</h3>
+				<h3>Next.JS</h3>
+				<h3>Blockchain</h3>
+			</nav>
 			<main>{children}</main>
 			<footer>All rights reserved</footer>
 		</>
